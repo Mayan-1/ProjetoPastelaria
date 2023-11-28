fda9557cbf775c65d3dcc8c8c5e4d7f235eb5f84
Mayan <may.santana197@gmail.com>
2023-11-28 17:15:35 -0300

    Inserindo dados na tabela clientes

---
+++ InsercaoDeDados.sql
@@ -0,0 +1,14 @@
+use pastelaria1;
+
+insert into clientes(nome_completo,nome_cadastro,nascimento,telefone,email,bairro,cidade,estado)
+values("David Eduardo dos Santos Ferreira", "David",'2001-09-16',"75984846551","davidedsf09@gmail.com","centro","terra nova","Bahia"),
+("Maria Eduarda dos Santos Ferreira", "Eduarda",'2008-05-16',"75984545451","mariaedsf09@gmail.com","centro","terra nova","Bahia"),
+("Carlos Eduardo dos Santos Ferreira", "Carlos",'2007-09-16',"759844451","carlosedsf09@gmail.com","centro","terra nova","Bahia"),
+("Taiana Silva", "Taiana","2001-05-29","75981111111","Taianasilva@gmail.com","centro","Feira de santana","Bahia"),
+("Jurema do mato", "Jurema","1995-02-15","75981222222","juremasmatos@gmail.com","centro","Feira de Santana","Bahia"),
+("thiguinho santista", "Thiago","2000-05-08","75982333333","thiaguin@gmail.com","centro","Feira de santana","Bahia"),
+("silvano sales", "silvano","1995-02-25","75984333333","silvano sales@gmail.com","new city","Feira de Santana","Bahia"),
+("tales silva", "Tales","1990-01-01","75984563210","talessilva@gmail.com","New Street","Feira de santana","Bahia"),
+("jorge bahia", "jorge","1995-02-15","75985647125","jorgebahia@gmail.com","Brokylin","Feira de Santana","Bahia"),
+("tayana teles", "tay","1997-05-08","75986324578","tay10@gmail.com","feira 1","Feira de santana","Bahia"),
+("roberval santos", "roberval","1990-05-05","75981111121","taliscasouza@gmail.com","centro","Feira de Santana","Bahia");
